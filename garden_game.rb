760e1254b22db55092fcfdfb18ca35bd6c0f8e03
Katalin-Anett Balazs <tk.anett@yahoo.com>
2019-04-08 19:01:03 +0100

    Update garden_game.rb

--- garden_game.rb
+++ garden_game.rb
@@ -1,16 +1,15 @@
 class Garden
-attr_accessor :name, :count, :water, :fruits, :collect
+attr_accessor :name, :count, :water, :fruits
 
   def initialize 
     @name = name
-    @count = 0
+    @count = 1
     @water = 0
     @fruits = 0
-    @collect = 0
   end
 
-  def plant name
-    puts "You just planted an #{name} tree. By watering it, you will get fruits to collect."
+  def plant 
+    puts "You just planted a tree. By watering it, you will get fruits to collect."
     @count += 1
     puts "You now have in total #{@count} trees."
   end
@@ -43,13 +42,15 @@ attr_accessor :name, :count, :water, :fruits, :collect
     elsif @water > 5 
       @fruits = 50 + rand(50)
       puts "You have #{@fruits} fruits."
-    else "You don't have fruits to collect!"
+    else puts "You don't have fruits to collect!"
       @fruits = 0
     end
   end
 
   def collect
-    puts "You can collect #{@fruits} fruits. "
+    puts "You collected #{@fruits} fruits."
+    @fruits = 0
+    @water = 0
   end
 
 end
@@ -57,52 +58,34 @@ end
 puts "Hello and welcome to a gardening game!"
 puts "You can plant trees, water them so you can get fruits to collect."
 puts "The options you can choose from:\n
-1.What tree you want to plant
+1.Plant a tree
 2.Water the tree/trees
 3.See how many trees you have
 4.See how many fruits you have
 5.Collect your fruits"
 puts "Let's get started then!"
 puts "-"*30
+
+puts "Let's plant a tree first. When you water your trees, you get fruits from them."
 tree = Garden.new
-puts "What would you like to plant first?"
-input = gets.chomp!
-tree.plant input
-puts "What would you like to do now? (Enter a number from 1 to 5 )"
-answer = gets.chomp!
 
-# tried to do a loop so the game would continue to go
-def loop input
-puts "What would you like to do now? (Enter a number from 1 to 5 )"
-answer = gets.chomp!
-end
 
-case answer
-    when "1"
-      puts "What tree do you want to plant?"
-      name = gets.chomp!
-      tree.plant name
-      puts "What would you like to do now?"
-      input = gets.chomp!
-      loop input
-    when "2"
-      tree.water
-      puts "What would you like to do now?"
-      input = gets.chomp!
-      loop input
-    when "3"
-      tree.trees
-      puts "What would you like to do now?"
-      input = gets.chomp!
-      loop input
-    when "4"
-      tree.fruits
-      puts "What would you like to do now?"
-      input = gets.chomp!
-      loop input
-    when "5"
-      tree.collect
-      puts "What would you like to do now?"
-      input = gets.chomp!
-      loop input
+while true
+  puts
+  puts "What would you like to do now? (Enter a number from 1 to 5 )"
+  answer = gets.chomp!
+
+  if answer == "1"
+    tree.plant
+  elsif answer == "2"
+    tree.water
+  elsif answer == "3"
+    tree.trees
+  elsif answer == "4"
+    tree.fruits
+  elsif answer == "5"
+    tree.collect
+  else
+    puts "Please type one of the commands."
+  end
 end
