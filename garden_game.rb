4fa0b0db28d4afd6e6820c060501cd38c53311b2
SinnahSaint <sinnah.saint@gmail.com>
2020-07-16 21:12:07 -0400

    changed all the "fruits" to "fruit" because it was driving me bonkers
    Done for now. Just needed some code to refactor for my ruby course. Thanks for sharing!

--- garden_game.rb
+++ garden_game.rb
@@ -1,14 +1,14 @@
 class Garden
-  attr_reader :trees, :water, :fruits
+  attr_reader :trees, :water, :fruit
 
   def initialize
     @trees = 1
     @water = 0
-    @fruits = 0
+    @fruit = 0
   end
 
   def plant
-    puts "You just planted a tree. By watering it, you will get fruits to collect."
+    puts "You just planted a tree. By watering it, you will get fruit to collect."
     @trees += 1
     puts "You now have in total #{trees} trees."
   end
@@ -18,30 +18,30 @@ class Garden
   end
 
   def add_water
-    puts "You just watered your trees and maybe can collect some fruits!"
+    puts "You just watered your trees and maybe can collect some fruit!"
     @water += 1
   end
 
   def count_fruit
     grow_fruit
-    if @fruits == 0 
-      puts "You don't have fruits to collect!"
+    if @fruit == 0 
+      puts "You don't have fruit to collect!"
     else 
-      puts "You have #{fruits} fruits."
+      puts "You have #{fruit} fruit."
     end
   end
   
   def grow_fruit
     case(water)
-    when 1..4 then @fruits = water * 5
-    when 5    then @fruits = 30
-    else @fruits = 50 + rand(50)
+    when 1..4 then @fruit = water * 5
+    when 5    then @fruit = 30
+    else @fruit = 50 + rand(50)
     end
   end
   
   def collect
-    puts "You collected #{fruits} fruits."
-    @fruits = 0
+    puts "You collected #{fruit} fruit."
+    @fruit = 0
     @water = 0
   end
 end
@@ -50,18 +50,18 @@ def welcome
   puts <<~HEREDOC
         Hello and welcome to a gardening game!
 
-        You can plant trees, water them so you can get fruits to collect.
+        You can plant trees, water them so you can get fruit to collect.
         The options you can choose from:
 
           1.Plant a tree
           2.Water the tree/trees
           3.See how many trees you have
-          4.See how many fruits you have
-          5.Collect your fruits
+          4.See how many fruit you have
+          5.Collect your fruit
 
         Let's get started then!
         #{"-" * 30}
-       You start with one tree. When you water your trees, you get fruits from them. 
+       You start with one tree. When you water your trees, you get fruit from them. 
 HEREDOC
 end
 
